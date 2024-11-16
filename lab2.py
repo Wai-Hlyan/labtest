f3fe3b82982ccc7ba57fd5048b06d9de1b64620b
WaiHlyan <waihlyan.23@ichat.sp.edu.sg>
2024-11-16 14:55:15 +0800

    Added other functions

--- lab2.py
+++ lab2.py
@@ -22,7 +22,11 @@ def calculate_bmi(height, weight):
 def display_main_menu():
     print("Python practice")
     userInput = get_user_input()
+    sorted_list = sort_temperature(userInput)
     calc_avg(userInput)
+    find_min_max(userInput)
+    sort_temperature(userInput)
+    calc_medium(sorted_list)
     calculate_bmi(57, 1.73)
 
 def get_user_input():
@@ -39,7 +43,27 @@ def calc_avg(num):
         total += i
     avg =  total / len(num)
     print(f'Average = {avg}')
-    return avg
+    
+def find_min_max(list):
+    maxNum = max(list)
+    minNum = min(list)
+    print(f"Maximum Number is: {maxNum}")
+    print(f"Minimum Number is: {minNum}")
+    
+def sort_temperature(list):
+    sortedList = sorted(list)
+    print(f"Sorted list is : {sortedList}")
+    return sortedList
+
+def calc_medium(list):
+    length = len(list)
+    mid = length // 2
+    
+    if length%2 == 1:
+        medium = (list[mid-1] + list[mid]) / 2
+        print(f"The medium number is: {medium}")
+    elif length%2 == 0:
+        print(f"The medium number is: {list[mid]}")
 
 if __name__ == "__main__":
     main()

1c7fe175a81d65bcccbf296f0e99d270688eaafb
WaiHlyan <waihlyan.23@ichat.sp.edu.sg>
2024-11-16 14:41:26 +0800

    bug fixed

--- lab2.py
+++ lab2.py
@@ -1,7 +1,8 @@
 #practicing python
 
 def main():
-    calculate_bmi(57, 1.73)
+    display_main_menu()
+
 
 
 def calculate_bmi(height, weight):
@@ -18,7 +19,27 @@ def calculate_bmi(height, weight):
     elif bmi > 25.0:
         print("Over Weight")
 
+def display_main_menu():
+    print("Python practice")
+    userInput = get_user_input()
+    calc_avg(userInput)
+    calculate_bmi(57, 1.73)
 
+def get_user_input():
+    user_input = input()  
+    formatted_input = user_input.split(",")
+    float_list = [float(item) for item in formatted_input]
+    print("Converted to float list:", float_list)
+    return float_list 
+
+
+def calc_avg(num):
+    total = 0
+    for i in num:
+        total += i
+    avg =  total / len(num)
+    print(f'Average = {avg}')
+    return avg
 
 if __name__ == "__main__":
     main()
